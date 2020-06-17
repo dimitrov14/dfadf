7ed14c85a15d873fdccc99b25a139319e7cfa8ad
haker3331 <48095520+haker3331@users.noreply.github.com>
2020-06-17 09:37:37 +0300

    Initial commit

---
+++ Problem_1.R
@@ -0,0 +1,1 @@
+students <- read.csv("https://raw.githubusercontent.com/feb-uni-sofia/econometrics2020-solutions/master/data/StudentsPerformance.csv", stringsAsFactors = FALSE)
---
+++ Problem_2.R
@@ -0,0 +1,1 @@
+bikes <- read.csv("https://raw.githubusercontent.com/feb-uni-sofia/econometrics2020-solutions/master/data/bikes.csv", stringsAsFactors = FALSE)
